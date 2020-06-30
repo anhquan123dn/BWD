7a263963e662401fa488d90150675c22dd9ca626
anhquan123dn <taquansict@gmail.com>
2020-06-30 21:45:23 +0700

    repair

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const signUpButton = document.getElementById('to-sign-up');
-const signInButton = document.getElementById('to-sign-in');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
-signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
-
-
